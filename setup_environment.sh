6f18a4fabf1435942ee3d43bab99e40fe2517530
hawa1222 <hw97.business@proton.me>
2024-04-05 23:49:06 +0100

    Chnage in setup_environment.sh

--- setup_environment.sh
+++ setup_environment.sh
@@ -3,25 +3,32 @@
 # Stop on the first sign of trouble
 set -e
 
-# Create a new conda environment named rtpenv with Python 3.8
-echo "Creating a new Conda environment named rtpenv with Python 3.9..."
-conda create --name rtpenv python=3.9 -y
+# Print a message to indicate the start of the environment setup
+echo "Starting environment setup..."
+
+# Create a new Python virtual environment named rtpenv in the current directory
+echo "Creating a new virtual environment named rtpenv with Python 3.9..."
+python3 -m venv rtpenv
 
 # Activate the newly created environment
+# Note: The method of activation depends on your shell. The below command works for bash/sh.
 echo "Activating the rtpenv environment..."
-conda activate rtpenv
+source rtpenv/bin/activate
 
-echo "Installing spyder-kernels for compatibility with Spyder..."
-conda install spyder-kernels=2.4 -y
+# Update pip to its latest version within the virtual environment
+echo "Upgrading pip to the latest version..."
+pip install --upgrade pip
 
 # Install Python dependencies using pip
+echo "Installing Python dependencies from the main requirements.txt..."
 pip install -r /Users/hadid/GitHub/real-time-data-processing/requirements.txt
 
+# Below commands install dependencies from those additional requirements.txt files
 echo "Installing Python dependencies from Kafka requirements.txt..."
 pip install -r /Users/hadid/GitHub/real-time-data-processing/kafka/requirements.txt
 
 echo "Installing Python dependencies from Spark requirements.txt..."
 pip install -r /Users/hadid/GitHub/real-time-data-processing/spark/requirements.txt
 
-echo "Environment setup is complete."
-
+# Indicate the completion of the environment setup
+echo "Environment setup is complete. The virtual environment 'rtpenv' is now ready for use."
